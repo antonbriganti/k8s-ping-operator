e4cafa64667452a8a228f486eb1214c8a5cf6bed
Anton Briganti <anton.briganti@gmail.com>
2021-06-17 15:34:13 +1000

    Added build and publish script
    This script builds the docker image, tags it with latest as well as the
    corresponding git commit hash, and then pushes to dockerhub.
    
    Would be helpful in a pipeline, probably need to update with `set` and
    such to make it better to use.

---
+++ bin/build-and-publish.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+
+COMMIT_SHA=$(git rev-parse HEAD)
+
+docker build -t antonbriganti/pingtest-operator:$COMMIT_SHA --target app .
+docker tag antonbriganti/pingtest-operator:$COMMIT_SHA antonbriganti/pingtest-operator:latest
+docker push antonbriganti/pingtest-operator:$COMMIT_SHA
+docker push antonbriganti/pingtest-operator:latest
